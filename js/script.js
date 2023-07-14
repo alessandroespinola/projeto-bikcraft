dd2b8c4c7db0c2b7659e8aae382cbc8435475695
alessandroespinola <117318876+alessandroespinola@users.noreply.github.com>
2023-07-14 11:57:57 -0400

    Atualizado url em javScript 2/7

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//ativar links do menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 
@@ -12,9 +14,21 @@ function ativarLink(link) {
 }
 
 
-
-
-
-
-
 links.forEach(ativarLink);
+
+
+//ativar itens do orçamento
+
+
+const parametros = new URLSearchParams(location.search) ;
+
+
+    function ativarProduto(parametro) {
+        const elemento = document.getElementById(parametro) ;
+        if (elemento) {
+        elemento.checked = true;
+        }
+    }
+    
+
+    parametros.forEach(ativarProduto);
